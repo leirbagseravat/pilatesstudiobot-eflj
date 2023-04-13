3c21d645ddba6e7a9bd84270a5337262d68c2ff0
Glitch (hello-express) <none>
2023-04-13 09:57:08 -0300

    adding project

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 
@@ -13,94 +8,210 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
-
-const objetoTempo = require('openweathermap-node');
-
-// For temperature in Fahrenheit use units=imperial
-// For temperature in Celsius use units=metric
-const helper = new objetoTempo(
-	{
-		APPID: 'd8ff33461e38363901c7cc712d49dca1',
-		units: "metric"
-	}
-);
-
-
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
+const credentials = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore({ projectId: credentials.project_id, credentials });
+const clientsRef = db.collection('clients');
 
-app.post("/aulaPLN", function(request, response) {
 
- //response.json({"fulfillmentText" : "Previsao do xxxx tempo agora..."});
-  
- var intentName = request.body.queryResult.intent.displayName;
-  
- if (intentName == "temperatura") {
-   
-    var cidade  = request.body.queryResult.parameters['cidade'];
+    function addAppointmentToClient(request, response) {
+        const person = request.body.queryResult.parameters.person;
+        const phone = request.body.queryResult.parameters.telefone;
 
-    //response.json({"fulfillmentText" : "Previsao do tempo para " + cidade});
-// }
-    helper.getCurrentWeatherByCityName("" + cidade, (err, currentWeather) => {
-	    if (err) {
-		    console.log(err);
-        
-        response.json({"fulfillmentText": "Cidade ''" +  cidade + " '' nao encontrada"});
-	    }
-	    else {
-	     	console.log(currentWeather);
-        
-         var temperaturaAtual  = currentWeather.main.temp;
-         var temperaturaMaxima = parseInt(currentWeather.main.temp_max);
-         var temperaturaMinima = parseInt(currentWeather.main.temp_min);
-       
-        
-        /*response.json({"fulfillmentText" :
-          "Cidade: " + currentWeather.name + "\n" +
-          "Temperatura Atual: " + temperaturaAtual + "º" + "\n" +
-          "Temperatura Máxima: " + temperaturaMaxima + "º" + "\n" +
-          "Temperatura Mínima: " + temperaturaMinima
+        let day = new Date(request.body.queryResult.parameters.dia);
+        let hour = new Date(request.body.queryResult.parameters.horario);
 
-        });*/
-        response.json({"fulfillmentMessages":
-           [
-            {
-              "card": {
-                 "title": "Previsão do Tempo",
-                 "subtitle": "Cidade = " + currentWeather.name,
-                  "imageUri": "https://cdn.glitch.com/ed90767e-7d31-49a0-944f-1e1f4f07b572%2Fprevisao.png?v=1615465390523"
-              }
-            },
-            {
-             "text" :{
-                "text": ["Temperatura atual = " + temperaturaAtual + "º"]
-             }
-            },
-            {
-             "text" :{
-                "text": ["Temperatura máxima = " + temperaturaMaxima + "º"]
-             }
-            },
-            {
-             "text" :{
-                "text": ["Temperatura mínima = " + temperaturaMinima + "º"]
-             }
+        day.setHours(hour.getHours(), hour.getMinutes());
+
+        return clientsRef.where('phone', '==', phone).get().then((documents) => {
+            if (documents.size > 0) {
+                documents.forEach((doc) => {
+                    const res = doc.data();
+                    let appoint = res.appointments;
+                    appoint.push(day);
+                    clientsRef.doc(doc.id).update({ appointments: appoint });
+                });
+            } else {
+                clientsRef.add({name: person.name, appointments: [day], phone: phone});
             }
-             
-           ]
+
+            return response.json({
+                "fulfillment_messages": [{
+                    "payload": {
+                        "richContent": [
+                            [{
+                                "title": "Agendamento criado",
+                                "type": "description",
+                                "text": [
+                                    'Perfeito! ' + person.name + ' foi agendado com sucesso no dia \n\n\n'+ day.toLocaleString('es-ES', {timeZone: 'America/Sao_Paulo'})+ ' e o telefone cadastrado na reserva foi o ' + phone
+                                ]
+                            }]
+                        ]
+                    }
+                }]});
         });
-        
-	    }
-    });
-  }
+    }
+
+    function listAppointments(request, response) {
+        const phone = request.body.queryResult.parameters.telefone;
+
+        return clientsRef.where('phone', '==', phone).get().then((documents) => {
+            if (documents.size > 0) {
+                documents.forEach((doc) => {
+                    const res = doc.data();
+                    let appointments = res.appointments;
+
+                    if(appointments.length > 0) {
+                        let response = 'Os seguintes horários foram agendados neste telefone:';
+                        for (var appoint in appointments) {
+                            response = response + '\n\n' + appointments[appoint].toDate().toLocaleString('es-ES', { timeZone: 'America/Sao_Paulo' });
+                        }
+                        return response.json({
+                            "fulfillment_messages": [{
+                                "payload": {
+                                    "richContent": [
+                                        [{
+                                            "title": "Agendamentos",
+                                            "type": "description",
+                                            "text": [
+                                                response
+                                            ]
+                                        }]
+                                    ]
+                                }
+                            }]});
+                    } else {
+                        return response.json({
+                            "fulfillment_messages": [{
+                                "payload": {
+                                    "richContent": [
+                                        [{
+                                            "title": "Agendamentos não encontrado",
+                                            "type": "description",
+                                            "text": [
+                                                `Nenhum horário foi reservado para o número informado.  Digite "Agendar aula" para criar um apontamento`                                            ]
+                                        }]
+                                    ]
+                                }
+                            }]});
+                    }
+                });
+            } else {
+                return response.json({
+                    "fulfillment_messages": [{
+                        "payload": {
+                            "richContent": [
+                                [{
+                                    "title": "Usuário não encontrado",
+                                    "type": "description",
+                                    "text": [
+                                        `Desculpe, não achamos nenhum cadastro com o número informado. Digite "Agendar aula" para criar um apontamento`                                            ]
+                                }]
+                            ]
+                        }
+                    }]});            }
+        });
+    }
+
+
+    function deleteAppointment(request, response) {
+        const phone = request.body.queryResult.parameters['phone-number'];
+        const dia = request.body.queryResult.parameters.dia;
+        const horario = request.body.queryResult.parameters.horario;
+
+        let targetDate = new Date(dia);
+        let targetHour = new Date(horario);
+        targetDate.setHours(targetHour.getHours(), targetHour.getMinutes());
+
+        return clientsRef.where('phone', '==', phone).get().then((documents) => {
+            if (documents.size > 0) {
+                documents.forEach((doc) => {
+                    const res = doc.data();
+                    let appointments = res.appointments;
+
+                    let indexToDelete = -1;
+                    for (let i = 0; i < appointments.length; i++) {
+                        if (appointments[i].getTime() === targetDate.getTime()) {
+                            indexToDelete = i;
+                            break;
+                        }
+                    }
+
+                    if (indexToDelete >= 0) {
+                        appointments.splice(indexToDelete, 1);
+                        clientsRef.doc(doc.id).update({ appointments: appointments });
+                        return response.json({
+                            "fulfillment_messages": [{
+                                "payload": {
+                                    "richContent": [
+                                        [{
+                                            "title": "Agendamento cancelado com sucesso",
+                                            "type": "description",
+                                            "text": [
+                                                `O agendamento no dia ${targetDate.toLocaleString('es-ES', { timeZone: 'America/Sao_Paulo' })} foi cancelado com sucesso.`                                         ]
+                                        }]
+                                    ]
+                                }
+                            }]});
+                    } else {
+                        return response.json({
+                            "fulfillment_messages": [{
+                                "payload": {
+                                    "richContent": [
+                                        [{
+                                            "title": "Agendamento não encontrado",
+                                            "type": "description",
+                                            "text": [
+                                                `Não foi encontrado nenhum agendamento no dia ${targetDate.toLocaleString('es-ES', { timeZone: 'America/Sao_Paulo' })}.`                                            ]
+                                        }]
+                                    ]
+                                }
+                            }]});
+                    }
+                });
+            } else {
+                return response.json({
+                    "fulfillment_messages": [{
+                        "payload": {
+                            "richContent": [
+                                [{
+                                    "title": "Usuário não encontrado",
+                                    "type": "description",
+                                    "text": [
+                                        `Desculpe, não achamos nenhum cadastro com o número informado. Digite "Agendar aula" para criar um apontamento`                                            ]
+                                }]
+                            ]
+                        }
+                    }]});
+
+            }
+        });
+    }
+app.post("/pilatesStudio", function(request, response) {
+ var intentName = request.body.queryResult.intent.displayName;
+
+    if (intentName === "Agendar") {
+        return addAppointmentToClient(request, response);
+    }
+
+    if (intentName === "Listar") {
+        return listAppointments(request, response);
+    }
+
+    if (intentName === "DeletetarAgendamento") {
+        return deleteAppointment(request, response);
+    }
+
+    if(intentName === "Default Fallback Intent"){
+        logData("ERROR",`Interação nao esperada: ${request.body.queryResult.queryText}`)
+    }
 
 });
 
